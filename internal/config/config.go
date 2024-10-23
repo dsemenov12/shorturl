b048f9cd39b584280625ba4400a71d2cc9929731
Semenov, Dmitriy <dsemenov@tdera.ru>
2024-10-23 16:17:53 +0300

    Исправлены ошибки автотестов

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ var FlagRunAddr string
 var FlagBaseAddr string
 
 func ParseFlags() {
-    flag.StringVar(&FlagRunAddr, "a", ":8080", "адрес запуска HTTP-сервера")
+    flag.StringVar(&FlagRunAddr, "a", "127.0.0.1:8080", "адрес запуска HTTP-сервера")
 	flag.StringVar(&FlagBaseAddr, "b", "", "базовый адрес результирующего сокращённого URL")
 	
     flag.Parse()
